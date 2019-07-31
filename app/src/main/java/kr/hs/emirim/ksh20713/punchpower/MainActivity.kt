c6d156fd65aa608ec72ec38628b1dc5352c0e159
Suhyeon Kim <s2018s26@e-mirim.hs.kr>
2019-07-31 13:24:11 +0900

    Init Project

---
+++ app/src/main/java/kr/hs/emirim/ksh20713/punchpower/MainActivity.kt
@@ -0,0 +1,12 @@
+package kr.hs.emirim.ksh20713.punchpower
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
